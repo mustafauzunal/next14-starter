7c5f975a441c2d6f3dc7086abffeb5ee73d36a4e
MUSTAFA UZUNAL <mustafa.uzunal@yandex.com>
2024-09-22 21:56:33 +0300

    stylings are done

---
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./navLink.module.css";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({ item }) => {
+  const pathName = usePathname();
+  return (
+    <div
+      className={`${styles.container} ${
+        pathName === item.path && styles.active
+      }`}
+    >
+      <Link href={item.path}>{item.title}</Link>
+    </div>
+  );
+};
+
+export default NavLink;
