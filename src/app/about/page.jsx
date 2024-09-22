089c250d69297bcd5bc777ec43d2dbb26a891884
MUSTAFA UZUNAL <mustafa.uzunal@yandex.com>
2024-09-22 22:25:44 +0300

    Images added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/5847663/pexels-photo-5847663.jpeg"
+          alt="about"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
